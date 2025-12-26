77d6b040f03a5d15cf4908ee8f9c8e05850886b9
martyan <martin.juzl@gmail.com>
2025-12-26 14:35:19 +0100

    fix: types export

--- vite.config.library.ts
+++ vite.config.library.ts
@@ -4,7 +4,14 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      insertTypesEntry: true,
+      exclude: ['examples/**/*'],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
